ab666d09fb4a61135a7e281848db2eb8ee3d7023
aidanh911 <67652153+aidanh911@users.noreply.github.com>
2023-04-13 01:04:03 -0500

    Add files via upload
    adding separate branch to show task 1.2

--- First-Js-App/js/scripts.js
+++ First-Js-App/js/scripts.js
@@ -1,4 +1,14 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+/* alert('Hello World');
+
+let favoriteFood = 'Yakitori';
+
+document.write(favoriteFood);
+
+ */
+
+let pokemonList = [
+    {name: 'Bulbasaur', type: 'Grass', height: '7'},
+    {name: 'Charmander', type: 'Fire', height: '6'},
+    {name: 'Caterpie', type: 'Bug', height: '3'}
+];
+
